05b0ebfe8180b199eed190a6e944d357029a15a5
bogdandodbarenko <bodya.bodnarenko@gmail.com>
2020-03-10 15:23:05 +0200

    small style changes

--- src/modules/register/ui/RegisterView.tsx
+++ src/modules/register/ui/RegisterView.tsx
@@ -1,9 +1,12 @@
 import * as React from "react";
 import {Field, Form, Formik, FormikHelpers} from "formik";
-import {Button} from "@material-ui/core";
+import {Button, Typography} from "@material-ui/core";
+import {Link} from "react-router-dom";
 
 import {TextFormField} from "../../../components/";
 import {RegisterValues, validUserSchema} from "../types";
+
+import "./styles.css";
 
 interface Props {
   onSubmit: (
@@ -14,44 +17,58 @@ interface Props {
 
 const RegisterView: React.FC<Props> = ({ onSubmit }: Props) => {
   return (
-    <Formik
-      validationSchema={validUserSchema}
-      initialValues={{ firstName: "", lastName: "", email: "", password: "" }}
-      onSubmit={onSubmit}
-    >
-      {({ isSubmitting }) => (
-        <Form>
-          <Field
-            name={"firstName"}
-            label={"First Name"}
-            component={TextFormField}
-            variant="outlined"
-          />
-          <Field
-            name={"lastName"}
-            label={"Last Name"}
-            component={TextFormField}
-            variant="outlined"
-          />
-          <Field
-            name={"email"}
-            label={"Email"}
-            component={TextFormField}
-            variant="outlined"
-          />
-          <Field
-            name={"password"}
-            label={"Password"}
-            type={"password"}
-            component={TextFormField}
-            variant="outlined"
-          />
-          <Button disabled={isSubmitting} type="submit" variant={"outlined"}>
-            submit
-          </Button>
-        </Form>
-      )}
-    </Formik>
+    <div className={"register-container"}>
+      <Formik
+        validationSchema={validUserSchema}
+        initialValues={{ firstName: "", lastName: "", email: "", password: "" }}
+        onSubmit={onSubmit}
+      >
+        {({ isSubmitting }) => (
+          <Form className={"register-form"}>
+            <Typography variant={"h2"}>Registration</Typography>
+            <Field
+              name={"firstName"}
+              label={"First Name"}
+              component={TextFormField}
+              variant="outlined"
+            />
+            <Field
+              name={"lastName"}
+              label={"Last Name"}
+              component={TextFormField}
+              variant="outlined"
+            />
+            <Field
+              name={"email"}
+              label={"Email"}
+              component={TextFormField}
+              variant="outlined"
+            />
+            <Field
+              name={"password"}
+              label={"Password"}
+              type={"password"}
+              component={TextFormField}
+              variant="outlined"
+            />
+            <Button
+              disabled={isSubmitting}
+              type="submit"
+              variant={"contained"}
+              fullWidth
+              color={"primary"}
+            >
+              Register
+            </Button>
+            <div style={{ paddingTop: 20 }}>
+              <Typography>
+                Already a user? Please <Link to={"/login"}>log in</Link>
+              </Typography>
+            </div>
+          </Form>
+        )}
+      </Formik>
+    </div>
   );
 };
 
