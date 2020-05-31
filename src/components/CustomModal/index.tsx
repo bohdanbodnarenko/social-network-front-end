b2e89d2e43fd8cce1101388df256ebf56bc74750
Bogdan <bodya.bodnarenko@gmail.com>
2020-05-31 15:51:13 +0300

    added custom modal

---
+++ src/components/CustomModal/index.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
+import Backdrop from "@material-ui/core/Backdrop";
+import Fade from "@material-ui/core/Fade";
+
+const useStyles = makeStyles((theme) => ({
+  modal: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
+interface Props {
+  open: boolean;
+  onClose: (event: any) => void;
+}
+
+export const CustomModal: React.FC<Props> = ({ open, onClose, children }) => {
+  const classes = useStyles();
+
+  return (
+    <Modal
+      aria-labelledby="modal"
+      className={classes.modal}
+      open={open}
+      onClose={onClose}
+      closeAfterTransition
+      BackdropComponent={Backdrop}
+      BackdropProps={{
+        timeout: 500,
+      }}
+    >
+      <Fade in={open}>{children as any}</Fade>
+    </Modal>
+  );
+};
